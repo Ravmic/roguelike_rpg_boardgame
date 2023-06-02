fd64cce0848ad1e7e31ee6e5f0d206b65ba75269
Rafał Michalski <raf.mic2@gmail.com>
2023-06-02 16:17:11 +0200

    Added new events to events objects

--- js/templeEv.js
+++ js/templeEv.js
@@ -2,11 +2,10 @@ export class Temple {
     constructor(luck, player) {
         this.currentPlayer = player
         this.luck = luck
-        this.vgoodEv = ['vgood']
-        this.goodEv = ['good']
-        this.neutralEv = ['neutral']
-        this.badEv = ['bad']
-        this.vbadEv = [`vbad`]
+        this.vgoodEv = [`I entered the temple and saw a blinding beam of light striking from the ceiling. Suddenly, an angel came from the sky and reached its hand out to me. ${this.currentPlayer.revive ? " I felt blessed." : " It gave me golden relic."}`]
+        this.goodEv = ['I met a friendly priest that helped me heal my wounds', `I enter the temple and kneel before a holy statue. I have been praying for a long time for strength and luck on my journey.`, `The gods heard me, brought me relief!`]
+        this.neutralEv = [`there's no one here...`, `This temple is empty`, `Sadly, this temple was destroyed long time ago...`]
+        this.vbadEv = [`I entered the temple and saw a mysterious figure behind the altar. Suddenly, the doors behind me locked, and the figure reached its hand in my direction. I passed out. When I woke up, there was no one there${this.currentPlayer.revive ? ", and I couldn't find my relic!" : "."}`]
 
         this.hp = null
         this.revive = null
@@ -15,17 +14,26 @@ export class Temple {
         this.comment = ""
         console.log(this.luck)
         this.randomizeEvent()
+
+        console.log(this.currentPlayer)
     }
 
     randomizeEvent() {
         //vgood
         if (this.luck < 2) {
             const randomTxtEv = Math.floor(Math.random() * this.vgoodEv.length)
-            this.hp = 2
+
             this.randomEv = this.vgoodEv[randomTxtEv]
             this.commentType = "vgood"
-            this.revive = 1
-            this.comment = 'You just found a relic!!!'
+
+            if (this.currentPlayer.revive) {
+                this.hp = 4
+                this.comment = `You have been healed for ${this.hp}hp points!!`
+            } else {
+                this.revive = 1
+                this.comment = 'You just found a relic!!!'
+            }
+
         } //neutral
         else if (this.luck >= 2 && this.luck < 5) {
             const randomTxtEv = Math.floor(Math.random() * this.neutralEv.length)
@@ -45,11 +53,16 @@ export class Temple {
         //vbad
         else if (this.luck >= 11) {
             const randomTxtEv = Math.floor(Math.random() * this.vbadEv.length)
-            this.hp = 0
-            this.revive = -1
+
             this.randomEv = this.vbadEv[randomTxtEv]
             this.commentType = "vbad"
-            this.comment = "You just lost you relic!"
+            if (this.currentPlayer.revive) {
+                this.revive = -1
+                this.comment = "You just lost you relic!"
+            } else {
+                this.comment = `?????`
+                this.hp = -1
+            }
 
         }
 
