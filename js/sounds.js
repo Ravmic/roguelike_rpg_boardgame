7db60d9b70e8c19785da27dbef23350ebb685ce8
Rafał Michalski <raf.mic2@gmail.com>
2023-06-05 14:48:50 +0200

    change volume to ctrl all sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,3 +1,4 @@
+import "../public/sounds/roll-effect.mp3"
 export class Sounds {
     constructor() {
         this.audioEl = document.querySelector(".game__music-mp3")
@@ -5,10 +6,25 @@ export class Sounds {
         this.controlBarHandle = document.querySelector(".volume-control__handle")
         this.clickCheck = false
         this.handlePosition = 0
-        this.mainVolume = null
+        this.mainVolume = 0.2
+
+        this.roll = new Audio("./sounds/roll-effect.mp3")
 
         this.init()
     }
+    //sounds 
+    rollSound() {
+        this.roll.play()
+        this.roll.volume = this.mainVolume / 3
+    }
+
+    //play song
+    playSong() {
+        this.audioEl.play()
+        this.audioEl.volume = this.mainVolume
+
+    }
+
     //click checker
     isClicked() {
         this.clickCheck = true
@@ -32,12 +48,15 @@ export class Sounds {
         this.calcOffset(clientX)
         const percent = this.handlePosition / this.controlBarEl.offsetWidth
         this.controlBarHandle.style.left = `${percent * 100}%`
-        this.audioEl.volume = percent
+        this.mainVolume = percent
+
+        this.audioEl.volume = this.mainVolume
+        this.roll.volume = this.mainVolume
+        console.log(this.mainVolume)
     }
+
     //Initialize playing song and listening to volume slider
     init() {
-        this.audioEl.play()
-        this.audioEl.volume = 0.2
 
         this.controlBarHandle.addEventListener('mousedown', () => this.isClicked())
         window.addEventListener('mouseup', () => this.isUnclicked())
